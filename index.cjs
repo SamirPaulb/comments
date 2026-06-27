50965b6725f0197293d7b596884ae1b68f61ea97
Samir Paul <77569653+SamirPaulb@users.noreply.github.com>
2026-06-28 03:47:37 +0530

    Add LinkInterceptor plugin with white and black lists
    Signed-off-by: Samir Paul <77569653+SamirPaulb@users.noreply.github.com>

--- index.cjs
+++ index.cjs
@@ -1,7 +1,21 @@
 const Application = require('@waline/vercel');
+const LinkInterceptor = require('@waline-plugins/link-interceptor');
 
 module.exports = Application({
-  plugins: [],
+  plugins: [
+    LinkInterceptor({
+      whiteList: [
+        'samir.pages.dev',
+        'samirpaulb.github.io',
+        'samirpaul.workers.dev',
+        'web.samirpaul.workers.dev'
+      ],
+      blackList: [
+        'baidu.com'
+      ],
+      // interceptorTemplate: `redirect to __URL`
+    });
+  ],
   async postSave(comment) {
     // do what ever you want after comment saved
   },
